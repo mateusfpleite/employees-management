c13f41222702087159197642eba2694b634df7c4
Mateus <mateusfpleite@gmail.com>
2024-12-08 09:52:26 -0300

    add new fields to employees table and add extension to unaccent names
    Co-authored-by: Heitor Gandolfi <heitor.gandolfi@hotmail..com>

--- db/migrate/20241208122634_create_employees.rb
+++ db/migrate/20241208122634_create_employees.rb
@@ -2,8 +2,9 @@ class CreateEmployees < ActiveRecord::Migration[8.0]
   def change
     create_table :employees do |t|
       t.string :name
-      t.string :role
+      t.string :role, array: true, default: []
       t.string :document
+      t.string :file
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_05_201656) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_08_122634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "unaccent"
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
-    t.string "role"
+    t.string "role", default: [], array: true
     t.string "document"
+    t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
